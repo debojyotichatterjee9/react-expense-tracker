091654543ffe8a587d60b20e2bfeb6302081cb80
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-04-11 00:47:41 +0530

    added css file to the dumb comp

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Person.css'
 const Person = (props) => {
     return (
-    <div>
+    <div className='Person'>
         <p>This person's name is {props.name}...with random number {props.number}</p>
         <p onClick={props.click}>{props.children}</p>
-        <input type="text" onChange={props.nameChange} defaultValue={props.name}/>
+        <input type='text' onChange={props.nameChange} defaultValue={props.name}/>
     </div>
     )
 }
