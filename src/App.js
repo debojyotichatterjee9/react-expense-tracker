2a39cfdc314ceb62b2607c04f005517231475933
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-04 17:22:01 +0530

    refreshed

--- src/App.js
+++ src/App.js
@@ -2,204 +2,29 @@
 import React from 'react';
 import './App.css';
 
-import Person from './Person/Person';
-import UserInput from './UserInput/UserInput'
-import UserOutput from './UserOutput/UserOutput'
 
-// const App = props => {
-
-//   const [personState, setPersonState] = useState({
-//     persons: [
-//       {
-//         name: "Mary",
-//         age: 34,
-//       },
-//       {
-//         name: "John",
-//         age: 25,
-//       },
-//       {
-//         name: "Carol",
-//         age: 30,
-//       },
-//       {
-//         name: "Juilan",
-//         age: 28,
-//       }
-//     ],
-//     zones: [
-//       {
-//         name: "India",
-//         code: "IN"
-//       },
-//       {
-//         name: "America",
-//         code: "US"
-//       }
-//     ]
-//   });
-
-//   const switchNameHandler = () => {
-//     setPersonState({
-//       persons: personState.persons.reverse()
-//     })
-//   }
-//   console.log(personState)
-//     return (
-//       <div className="App">
-//         <code>React App Page:</code>
-//         <hr />
-//         <button onClick={switchNameHandler}>Switch Names</button>
-//         <Person name={personState.persons[0].name} age={personState.persons[0].age} />
-//         <Person name={personState.persons[1].name} age={personState.persons[1].age} />
-//         <Person name={personState.persons[2].name} age={personState.persons[2].age}/>
-//         <Person 
-//         name={personState.persons[3].name} age={personState.persons[3].age}
-//         click={switchNameHandler}        
-//         >Click here to reverse.</Person>
-//       </div>
-//     );
-//   }
-
-  class App extends React.Component {
-
-    state = {
-      persons: [
-        {
-          name: "Mary",
-          number: 34,
-        },
-        {
-          name: "John",
-          number: 25,
-        },
-        {
-          name: "Carol",
-          number: 30,
-        },
-        {
-          name: "Juilan",
-          number: 28,
-        },
-        {
-          name: "Madeline",
-          number: 35,
-        },
-        {
-          name: "Jack",
-          number: 84,
-        }
-      ],
-      username: "fredbarner"
-    }
-
-  switchNameHandler = () => {
-    this.setState({
-      persons: this.state.persons.reverse()
-    })
-    console.log(this.state.persons)
-  }
-
-  increaseNumberHandler = () => {
-    const updatedPerson = this.state.persons.map(person => ({
-      name: person.name,
-      number: person.number + 1
-    }));
-    this.setState({
-      persons: updatedPerson
-    })
-  }
-
-  resetNumberHandler = (resetValue) => {
-    const updatedPerson = this.state.persons.map(person => ({
-      name: person.name,
-      number: resetValue
-    }));
-    this.setState({
-      persons: updatedPerson
-    })
-  }
-
-  removeNameHandler = (maskValue) => {
-    const updatedPerson = this.state.persons.map(person => ({
-      name: maskValue,
-      number: person.number
-    }));
-    this.setState({
-      persons: updatedPerson
-    })
-  }
-
-  updateNameHandler = (event) => {
-    console.log(event)
-    const updatedPerson = this.state.persons.map(person => ({
-      name: event.target.value,
-      number: person.number
-    }));
-    this.setState({
-      persons: updatedPerson
-    })
-  }
-
-  updateUsernameHandler = async(event) => {
-
-    await this.setState({
-      username: event.target.value
-    });
-    console.log(this.state.username)
-  }
-
-    render = () => {
-
-      return (
-
+const App = props => {
+    return (
       <div className="App">
-        <code>React App Page:</code>
-        <hr />
-        <button onClick={this.switchNameHandler}>Switch Names</button>
-        <Person 
-        name={this.state.persons[0].name} 
-        number={this.state.persons[0].number}
-        nameChange={this.updateNameHandler}
-        />
-        <Person 
-        name={this.state.persons[1].name} 
-        number={this.state.persons[1].number} 
-        nameChange={this.updateNameHandler}
-        />
-        <Person 
-        name={this.state.persons[2].name} 
-        number={this.state.persons[2].number} 
-        nameChange={this.updateNameHandler}
-        />
-        <Person 
-        name={this.state.persons[3].name} number={this.state.persons[3].number}
-        click={this.increaseNumberHandler.bind(this)}
-        nameChange={this.updateNameHandler}
-        >Click here to increase number.</Person>
-        <Person 
-        name={this.state.persons[4].name} number={this.state.persons[4].number}
-        click={this.resetNumberHandler.bind(this, 0)}
-        nameChange={this.updateNameHandler}
-        >Click here to reset all numbers.</Person>
-        <Person 
-        name={this.state.persons[5].name} number={this.state.persons[5].number}
-        click={() => this.removeNameHandler("XXX")}
-        nameChange={this.updateNameHandler}
-        >Click here to remove all the names.</Person>
-
-
-
-        <UserInput 
-        udpateUname={this.updateUsernameHandler}
-        uname={this.state.username}/>
-        <UserOutput
-        uname={this.state.username}/>
+        <h2>Refreshed</h2>
       </div>
-      
-      // React.createElement('div', {className: 'App'}, React.createElement('p', null, 'This is a test string'))
-      )
-    }
+    );
   }
+
+  // class App extends React.Component {
+
+  //   render = () => {
+
+  //     return (
+
+  //     <div className="App">
+  //       <code>React App Page:</code>
+  //       <hr/>
+  //       </div>
+      
+  //     // React.createElement('div', {className: 'App'}, React.createElement('p', null, 'This is a test string'))
+  //     )
+  //   }
+  // }
 
   export default App;
