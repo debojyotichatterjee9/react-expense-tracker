6c4d29dad3f78eae7497001d2c13146d2e47a6e6
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-05 01:19:37 +0530

    possible modifications

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,12 +1,24 @@
 import './ExpenseItem.css'
-export default function ExpenseItem () {
-    return(
+
+
+function ExpenseItem (props) {
+    const expenses = props.expenses;
+    console.log(expenses)
+    for(const item of expenses) {
+        console.log(item)
+        const expenseDate = item.date;
+        return(
         <div className="expense-item">
-            <div>March 20, 2021</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">1294.67</div>
+                <h2>{item.title}</h2>
+                <div className="expense-item__price">${item.amount}</div>
             </div>
         </div>
-        )
+        );
+    }
+    
 }
+
+
+export default ExpenseItem
