eb8b79e60be5573159d7a6cc28cef4b1df951b11
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-05 19:25:14 +0530

    list handling

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,12 +4,13 @@ import ExpenseItem from '../ExpenseItem/ExpenseItem'
 
 function Expenses (props) {
     const expenses = props.expenses
+    
+    const elemArray = expenses.map( item => {
+        return <ExpenseItem key={item.id} date={item.date} title={item.title} amount={item.amount}/>
+    });
         return(
             <Card className="expenses">
-                <ExpenseItem date={expenses[0].date} title={expenses[0].title} amount={expenses[0].amount}/>
-                <ExpenseItem date={expenses[1].date} title={expenses[1].title} amount={expenses[1].amount}/>
-                <ExpenseItem date={expenses[2].date} title={expenses[2].title} amount={expenses[2].amount}/>
-                <ExpenseItem date={expenses[3].date} title={expenses[3].title} amount={expenses[3].amount}/>
+                {elemArray}
             </Card>
         );
     
