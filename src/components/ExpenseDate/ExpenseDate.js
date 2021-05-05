c28c788788772e2bf6289ac0a73c29adae441b6c
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-05 16:08:14 +0530

    styling the date component

--- src/components/ExpenseDate/ExpenseDate.js
+++ src/components/ExpenseDate/ExpenseDate.js
@@ -4,10 +4,10 @@ import './ExpenseDate.css'
 function ExpenseDate (props) {
     const expenseDate = props.date;
         return(
-        <div>
-            <div>{expenseDate.toLocaleString('en-US', {month: 'long'})}</div>
-            <div>{expenseDate.getFullYear()}</div>
-            <div>{expenseDate.toLocaleString('en-US', {day: '2-digit'})}</div>
+        <div className="expense-date">
+            <div className="expense-date__month">{expenseDate.toLocaleString('en-US', {month: 'long'})}</div>
+            <div className="expense-date__year">{expenseDate.getFullYear()}</div>
+            <div className="expense-date__day">{expenseDate.toLocaleString('en-US', {day: '2-digit'})}</div>
         </div>
             
         );
