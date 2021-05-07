a90921067d51bf46e0d571aa65094afbe0890a2a
Debojyoti Chatterjee <debojyoti.chatterjee9@gmail.com>
2021-05-07 15:05:24 +0530

    chekcing useState function

--- src/components/ExpenseComponents/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseComponents/ExpenseItem/ExpenseItem.js
@@ -1,17 +1,23 @@
+import { useState } from 'react'
 import './ExpenseItem.css';
 import Card from '../../UIComponents/Card/Card';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 
 const ExpenseItem = props => {
+
+    const [title, setTitle] = useState(props.title)
+    
     const expenseDate = props.date;
+
         return(
         <Card className="expense-item">
             <ExpenseDate date={expenseDate}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
+            <button style={{"cursor": "pointer"}} onClick={() => setTitle(Math.round(Math.random(100)*100))}>Change Title</button>
         </Card>
         );
     
